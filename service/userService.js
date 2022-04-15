27947c4612789392742b48f2a3aaf722c0d24267
Elienai <elienay.soares07@gmail.com>
2022-04-14 22:17:51 -0300

    teste

--- service/userService.js
+++ service/userService.js
@@ -5,18 +5,18 @@ const checkUser = async (usuario) =>{
     const resultCheckEmail = await userDao.checkEmail(usuario)
 
     if (usuario.nm_name.length < 3) {
-        return { id:0, codigo: "Nome inválido."}
+      return { id:0, codigo: "Nome inválido."}
     } else if (usuario.cd_password < 8) {
-        return { id:0, codigo: "Senha pequena."}
+      return { id:0, codigo: "Senha pequena."}
     } else if (resultCheckEmail != null) {
-        return { id:0, codigo: "E-mail já cadastrado."}
+      return { id:0, codigo: "E-mail já cadastrado."}
     } else {
-        return { id:1, codigo: "Usuário cadastrado."}
+      return { id:1, codigo: "Usuário cadastrado."}
     }
 }
 
 const create = async (usuario) => {
-    const resultCheckUser = {id:1}
+    const resultCheckUser = await checkUser(usuario)
     console.log(resultCheckUser)
     if (resultCheckUser.id) { 
         resultCheckUser.cd_id = await userDao.cadastrar(usuario)
